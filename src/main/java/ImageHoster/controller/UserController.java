7732af78eefcb8b8fedc854a8bdff236227cd30e
Ashwini Ashish <ashwini.ashish@ril.com>
2021-01-18 19:22:52 +0530

    Feat: user regisration - password validation

--- src/main/java/ImageHoster/controller/UserController.java
+++ src/main/java/ImageHoster/controller/UserController.java
@@ -5,11 +5,13 @@ import ImageHoster.model.User;
 import ImageHoster.model.UserProfile;
 import ImageHoster.service.ImageService;
 import ImageHoster.service.UserService;
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
@@ -40,9 +42,14 @@ public class UserController {
     //This controller method is called when the request pattern is of type 'users/registration' and also the incoming request is of POST type
     //This method calls the business logic and after the user record is persisted in the database, directs to login page
     @RequestMapping(value = "users/registration", method = RequestMethod.POST)
-    public String registerUser(User user) {
-        userService.registerUser(user);
-        return "redirect:/users/login";
+    public String registerUser(User user, RedirectAttributes redirectAttributes) {
+        if (validatePassword(user.getPassword())) {
+            userService.registerUser(user);
+            return "redirect:/users/login";
+        }
+        String error = "Password must contain atleast 1 alphabet, 1 number & 1 special character";
+        redirectAttributes.addAttribute("passwordTypeError", error).addFlashAttribute("passwordTypeError", error);
+        return "redirect:/users/registration";
     }
 
     //This controller method is called when the request pattern is of type 'users/login'
@@ -78,5 +85,10 @@ public class UserController {
         List<Image> images = imageService.getAllImages();
         model.addAttribute("images", images);
         return "index";
+    }
+
+    private Boolean validatePassword(String pwd) {
+        String pwdPattern = "^(?=.*?[a-zA-Z])(?=.*?[0-9])(?=.*?\\W).+$";
+        return pwd.matches(pwdPattern);
     }
 }
